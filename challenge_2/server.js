8e0b00644272dc759bf7e0d182f1ec8a76ab85a8
Wendy Huynh <wendyhuynhs@gmail.com>
2019-05-07 17:01:05 -0700

    converted JSON to CSV

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
+const port = 3000;
 
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded());
+
+app.get('/api', (req, res) => {
+  res.status(200).send(csv);
+});
+
+app.post('/', (req, res) => {
+  let data = [];
+  data.push(JSON.parse(req.body.JSON));
+  let header = Object.keys(data[0]);
+  var replacer = function(key, value) {
+    return value === null ? '' : value;
+  };
+  let csv = data.map(row => {
+    return header
+      .map(columnName => {
+        return JSON.stringify(row[columnName], replacer);
+      })
+      .join(',');
+  });
+  csv.unshift(header.join(','));
+  console.log(csv);
+  res.status(200).send(csv);
+});
+
+app.use(express.static('client'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
